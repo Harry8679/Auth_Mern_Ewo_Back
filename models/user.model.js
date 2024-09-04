4deaa09350aa290ae373290be4e5cb9fb9d61f22
Harry EMARH <emarh.harry.code@gmail.com>
2024-09-04 23:48:59 +0200

    Some fields added

--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,33 @@ const userSchema = mongoose.Schema({
         type: String,
         required: [true, 'Please add a name']
     },
+    email: {
+        type: String,
+        required: [true, 'Please add a email'],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email",
+        ],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password']
+    },
+    photo: {
+        type: String,
+        required: [true, 'Please add a photo'],
+        default: 'https://i.ibb.co/4pDNDk1/avatar.png'
+    },
+    phone: {
+        type: String,
+        default: '+33'
+    },
+    bio: {
+        type: String,
+        default: 'bio'
+    },
 }, { 
     timestamps: true,
     minimize: false
